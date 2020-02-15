ad5fbd4dcd3438f68c8485806736e90fd17c3db8
DamiNo007 <dmoldabayev.99@gmail.com>
2020-02-15 17:18:58 +0600

    test

--- 2Week/CALCULATOR/app/src/main/java/com/example/calculator/MainActivity.java
+++ 2Week/CALCULATOR/app/src/main/java/com/example/calculator/MainActivity.java
@@ -1,12 +1,14 @@
 package com.example.calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.widget.ListViewAutoScrollHelper;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import java.lang.Math;
 
 import java.nio.DoubleBuffer;
 import java.security.interfaces.DSAPublicKey;
@@ -18,6 +20,17 @@ public class MainActivity extends AppCompatActivity {
    String lastOperation = "";
    String output = "";
 
+   public boolean isNumeric(String strNum){
+       if(strNum==null)
+           return false;
+       try{
+           double d = Double.parseDouble(strNum);
+       }catch (NumberFormatException e){
+           return false;
+       }
+       return true;
+   }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,25 +39,44 @@ public class MainActivity extends AppCompatActivity {
     }
 
     protected void onSaveInstanceState(Bundle outState){
-        outState.putString("OPERATION", lastOperation);
-        if(result!=null){
-            outState.putDouble("RESULT", result);
-        }
-        super.onSaveInstanceState(outState);
+       outState.putString("OPERATION", lastOperation);
+       outState.putString("INPUT", input.getText().toString());
+       if(result!=null){
+           outState.putDouble("RESULT",result);
+       }
+
+       super.onSaveInstanceState(outState);
     }
 
     protected void onRestoreInstanceState(Bundle savedInstanceState){
         super.onRestoreInstanceState(savedInstanceState);
+        String temp = savedInstanceState.getString("INPUT");
+
         lastOperation = savedInstanceState.getString("OPERATION");
-        result = savedInstanceState.getDouble("RESULT");
-        input.setText(result.toString());
-        input.setText(lastOperation);
+        if(isNumeric(temp)){
+            if(lastOperation.equals("=") || lastOperation.equals("sin") || lastOperation.equals("cos") || lastOperation.equals("tan") || lastOperation.equals("ln") || lastOperation.equals("!") || lastOperation.equals("π") || lastOperation.equals("e")){
+                result = Double.parseDouble(temp);
+                input.setText(result.toString());
+            }else{
+                if(!lastOperation.isEmpty())
+                    result = savedInstanceState.getDouble("RESULT");
+                str1 = temp;
+                input.setText(str1);
+            }
+        }
+        else{
+            result = savedInstanceState.getDouble("RESULT");
+            if(!lastOperation.equals("=") && !lastOperation.equals("sin") && !lastOperation.equals("C") && !lastOperation.equals("cos") && !lastOperation.equals("tan") && !lastOperation.equals("ln") && !lastOperation.equals("!") && !lastOperation.equals("sqrt") && !lastOperation.equals("π") && !lastOperation.equals("e")) {
+                input.setText(lastOperation);
+            }
+        }
     }
 
 
     public void onNumberClick(View v){
         input.setTextSize(40);
-        if(lastOperation.equals("=") && result!=null || lastOperation.equals("%") && result!=null || lastOperation.equals("x²") && result!=null || lastOperation.equals("√") && result!=null ) {
+        if(lastOperation.equals("=") && result!=null ||  lastOperation.equals("x²") && result!=null || lastOperation.equals("sqrt") && result!=null ||
+        lastOperation.equals("sin")  && result!=null || lastOperation.equals("cos") && result!=null || lastOperation.equals("tan")  && result!=null || lastOperation.equals("ln") && result!=null || lastOperation.equals("!") && result!=null || lastOperation.equals("π") && result!=null || lastOperation.equals("e") && result!=null) {
             result = null;
         }
         if(lastOperation.equals("C")){
@@ -62,6 +94,7 @@ public class MainActivity extends AppCompatActivity {
         }
         else{
             if(btn.getText().equals("DEL")){
+
                 if(str1.length()>0)
                     str1 = str1.substring(0,str1.length()-1);
             }
@@ -81,11 +114,13 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+
+
     public void onOperationClick(View v){
         input.setTextSize(40);
         Button btn = (Button)v;
         String operation = btn.getText().toString();
-        if(operation.equals("x²") || operation.equals("%") || operation.equals("√") || operation.equals("C")){
+        if(operation.equals("x²") || operation.equals("sqrt") || operation.equals("C") || operation.equals("sin") || operation.equals("cos") || operation.equals("tan") || operation.equals("ln") || operation.equals("!") || operation.equals("π") || operation.equals("e")){
             if(result!=null){
                 try{
                     performOperation(result,operation);
@@ -101,7 +136,8 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         }
-        if(!operation.equals("=") && !operation.equals("x²") && !operation.equals("%") && !operation.equals("√") && !operation.equals("C")){
+
+        if(!operation.equals("=") && !operation.equals("x²") && !operation.equals("sqrt") && !operation.equals("C") && !operation.equals("sin") && !operation.equals("cos") && !operation .equals("tan") && !operation.equals("ln") && !operation.equals("!") && !operation.equals("π") && !operation.equals("e")){
             input.setText(operation);
         }
 
@@ -138,18 +174,7 @@ public class MainActivity extends AppCompatActivity {
                     input.setText(output.toString());
                 }
             }
-            else if(operation.equals("%")){
-                result = a *0.01;
-                if(Math.round(result)==result){
-                    output = String.format("%.0f", result);
-                    input.setText(output.toString());
-                }
-                else{
-                    output = String.format("%.5f", result);
-                    input.setText(output.toString());
-                }
-            }
-            else if(operation.equals("√")){
+            else if(operation.equals("sqrt")){
                 result = Math.sqrt(a);
                 if(Math.round(result)==result){
                     output = String.format("%.0f", result);
@@ -160,11 +185,58 @@ public class MainActivity extends AppCompatActivity {
                     input.setText(output.toString());
                 }
             }
+            else if (operation.equals("!")){
+                double temp = a;
+                result = 1.0;
+                for (int i = 2; i<=temp; i++){
+                    result*=i;
+                }
+                input.setText(result.toString());
+            }
+            else if (operation.equals("sin")){
+                double temp = Math.toRadians(a);
+                result = Math.sin(temp);
+                input.setText(result.toString());
+            }
+            else if (operation.equals("cos")){
+                double temp = Math.toRadians(a);
+                result = Math.cos(temp);
+                input.setText(result.toString());
+            }
+            else if (operation.equals("tan")){
+                double temp = Math.toRadians(a);
+                result = Math.tan(temp);
+                input.setText(result.toString());
+            }
+            else if(operation.equals("ln")){
+                if(a==0.0){
+                    input.setTextSize(30);
+                    input.setText("Incorrect argument!");
+                    result = 0.0;
+                    lastOperation = "";
+                }
+                else{
+                    result = Math.log(a);
+                    input.setText(result.toString());
+                }
+
+            }
+            else if(operation.equals("π")){
+                result = a*Math.PI;
+                input.setText(result.toString());
+            }
+            else if(operation.equals("e")){
+                result = Math.exp(a);
+                input.setText(result.toString());
+            }
             else if(operation.equals("C")){
                 result = 0.0;
                 input.setText("");
             }
+
+
             else {
+                boolean isOk = true;
                 if(result == null){
                     result = a;
                 }
@@ -183,30 +255,73 @@ public class MainActivity extends AppCompatActivity {
                             case "÷":
                                 result /= a;
                                 break;
-                        }
 
-                        if(Math.round(result)==result){
-                            output = String.format("%.0f", result);
-                            input.setText(output.toString());
+                            case "%":
+                                result = (result*a)/100;
+                                break;
+                            case "^":
+                                result = Math.pow(result,a);
+                                break;
+                            case "√":
+                                result = Math.pow(result,1.0/a);
+                                break;
+                            case "log":
+                                if(result==0.0){
+                                    isOk =false;
+                                }
+                                else
+                                    result = Math.log(result)/Math.log(a);
+                                break;
                         }
-                        else{
-                            output = String.format("%.5f", result);
-                            input.setText(output.toString());
-                        }
+                            if(Math.round(result)==result){
+                                if(!isOk && lastOperation.equals("log")){
+                                    input.setTextSize(30);
+                                    input.setText("Incorrect argument!");
+                                    result = 0.0;
+                                    lastOperation = "";
+                                    isOk=true;
+                                }
+                                else{
+                                    output = String.format("%.0f", result);
+                                    input.setText(output.toString());
+                                }
+
+                            }
+                            else{
+                                output = String.format("%.5f", result);
+                                input.setText(output.toString());
+                            }
                     }
                     else{
                         switch(lastOperation) {
                             case "+":
                                 result += a;
                                 break;
-                            case "−":
-                                result -= a;
+                            case "-":
+                                result-=a;
                                 break;
                             case "×":
-                                result *= a;
+                                result*=a;
                                 break;
+
                             case "÷":
                                 result /= a;
+                                break;
+                            case "%":
+                                result = (result*a)/100;
+                                break;
+                            case "^":
+                                result = Math.pow(result,a);
+                                break;
+                            case "√":
+                                result = Math.pow(result,1.0/a);
+                                break;
+                            case "log":
+                                if(result==0.0){
+                                    isOk =false;
+                                }
+                                else
+                                    result = Math.log(result)/Math.log(a);
                                 break;
                         }
                     }
